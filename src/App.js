487c2e381ce4e2c4d965454907a2742c355bda41
DanielMaina <daniel_maina9@hotmail.com>
2020-10-15 22:27:42 -0400

    Revert "landing done"
    This reverts commit 86360dab6e3195e87e2a70692d3591a077d3935a.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-
-import Home from "./pages/Home";
-import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Route exact path="/" component={Home} />
-    </>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
